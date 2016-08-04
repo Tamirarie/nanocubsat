4992f9795abbd7caa598091ea85384f152acf0dc
Tamir Arie <tamirarie13@gmail.com>
2016-08-04 16:55:43 +0000

    adding names to temp

--- scripts/CPUtemp.sh
+++ scripts/CPUtemp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 zone3=$(cat /sys/class/thermal/thermal_zone3/temp)
-echo $("Thermal Zone3:"(zone3/1000))
+s1="Thermal Zone3: "
+s2="Thermal Zone4: "
+echo $s1 $((zone3/1000))
 zone4=$(cat /sys/class/thermal/thermal_zone4/temp)
-echo $("Thermal Zone4:"(zone4/1000))
+echo $s2 $((zone4/1000))
 
