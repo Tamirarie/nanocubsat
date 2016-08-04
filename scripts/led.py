72bd7d8a3e16efb5f71798c3c9169497fc38a533
Tamir Arie <tamirarie13@gmail.com>
2016-08-04 01:55:00 +0000

    modified led

--- scripts/led.py
+++ scripts/led.py
@@ -4,9 +4,10 @@
 import time
 import mraa
 
-while True:
-         led = mraa.Gpio(13)
+led = mraa.Gpio(13)
+while True:         
+	 time.sleep(0.5)
          led.write(1)
          ####defining time delay between blink
-         time.sleep(0.1)
+         time.sleep(0.5)
          led.write(0)
