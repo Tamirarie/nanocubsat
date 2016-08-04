4364ee2b772f9f098fe92b10d4b939d4dcb040ff
Tamir Arie <tamirarie13@gmail.com>
2016-08-04 16:47:40 +0000

    adding CPU TEMP

--- scripts/CPUtemp.sh
+++ scripts/CPUtemp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-zone3=$(/sys/class/thermal/thermal_zone3/temp)  
-zone4=$(/sys/class/thermal/thermal_zone4/temp)
-echo $(zone3/1000)
-echo $(zone4/1000)  
+zone3=$(cat /sys/class/thermal/thermal_zone3/temp)
+echo $("Thermal Zone3:"(zone3/1000))
+zone4=$(cat /sys/class/thermal/thermal_zone4/temp)
+echo $("Thermal Zone4:"(zone4/1000))
+
